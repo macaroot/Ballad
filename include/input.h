beec6f23e8ecc21922bfa0ef594e5182f2205299
macaroot <macankkuli@hotmail.com>
2016-10-24 17:23:04 +0300

    loop

---
+++ include/input.h
@@ -0,0 +1,22 @@
+/* This code is open-source.
+ * Images and music are copyrighted by Lasse Ilmonen / ilmoj (2016),
+ * and may not be used for commercial use without written permission.
+ * Fair Use applies, naturally.
+ */
+#pragma once
+#ifndef INPUT_H
+#define INPUT_H
+/*
+ * Variable declarations
+ */
+
+
+/*
+ * Function declarations
+ */
+// Manage all the input
+// Used by loop.c
+extern void inputManager();
+
+#endif
+
---
+++ include/loop.h
@@ -0,0 +1,22 @@
+/* This code is open-source.
+ * Images and music are copyrighted by Lasse Ilmonen / ilmoj (2016),
+ * and may not be used for commercial use without written permission.
+ * Fair Use applies, naturally.
+ */
+#pragma once
+#ifndef LOOP_H
+#define LOOP_H
+/*
+ * Variable declarations
+ */
+
+
+/*
+ * Function declarations
+ */
+// Everything in the game happens here
+// Used by game.c
+extern void theLoop();
+
+#endif
+
